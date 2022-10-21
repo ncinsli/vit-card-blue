5e7f9da9b885067fffc4ab449430e2b1db6f8e88
ncinsli <56553344+ncinsli@users.noreply.github.com>
2022-10-21 20:53:29 +0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+    theme: {
+      fontFamily: {
+        'Macondo': ['Macondo', 'Normal'],
+        'Inter' : ['Inter', 'normal']
+      },
+      extend: {},
+  },
+  plugins: [],
+}
